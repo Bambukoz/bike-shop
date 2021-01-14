07d284a589be027fc9a79a356c4b93e88e16970e
Ezhov Alexey <ezhov-a@yandex.ru>
2021-01-14 12:49:35 +0300

    Добавляет мобильное меню + скрипт для его работы

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,31 @@
 'use strict';
+
 (function () {
+  var header = document.querySelector('.js-header');
+  var btnMenuOpen = document.querySelector('.js-menu-open');
+
+  var headerClass = {
+    OPEN: 'header--open',
+    NOJS: 'header--nojs'
+  };
+
+  header.classList.remove(headerClass.NOJS);
+
+  var menuHandler = function (evt) {
+    if (evt.target === btnMenuOpen) {
+      header.classList.add(headerClass.OPEN);
+    } else {
+      header.classList.remove(headerClass.OPEN);
+    }
+  };
+
+  document.addEventListener('click', menuHandler);
+})();
+
+(function () {
+  var form = document.querySelector('.form');
+
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+  });
 })();

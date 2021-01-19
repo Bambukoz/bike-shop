3d3eef6e543a7402637ffaf5ca6c5e491616db0d
Ezhov Alexey <ezhov-a@yandex.ru>
2021-01-19 18:06:40 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -3,22 +3,18 @@
 (function () {
   var header = document.querySelector('.js-header');
   var btnMenuOpen = document.querySelector('.js-menu-open');
-  var body = document.querySelector('.js-page');
 
   var pageClass = {
     OPEN: 'header--open',
     NOJS: 'header--nojs',
-    BODY_LOCKED: 'page--locked'
   };
   header.classList.remove(pageClass.NOJS);
 
   var menuHandler = function (evt) {
     if (evt.target === btnMenuOpen) {
       header.classList.add(pageClass.OPEN);
-      body.classList.add(pageClass.BODY_LOCKED);
     } else {
       header.classList.remove(pageClass.OPEN);
-      body.classList.remove(pageClass.BODY_LOCKED);
     }
   };
 
